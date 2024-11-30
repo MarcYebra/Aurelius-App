fe035a11aa2ab7da7ad821375e9fff560d88f75f
Marc Yebra <marcyebra4@gmail.com>
2024-11-30 17:10:29 -0600

    started with setting up the frontend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  include ActionController::MimeResponds
+  
   allow_browser versions: :modern
 end
